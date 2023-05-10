da912bfd9432dacd21a64b69e701d944e5f4d8ea
djanloo <zzibecu@gmail.com>
2023-05-10 13:48:53 +0200

    correction seems to work

--- README.md
+++ README.md
@@ -2,11 +2,18 @@
 Settings and tests for Spinnaker on PyNN
 
 ## Installation
+
+To install with pipenv run:
+```
+pipenv install
+pipenv shell
+python correct_and_install.py
+```
+
+## Log of errors
 I'm using Ubuntu Ubuntu 22.04.2 LTS and I had a few problems while installing sPyNNaker using [this guide](http://spinnakermanchester.github.io/spynnaker/6.0.0/PyNNOnSpinnakerInstall.html) for spinnaker 6.0.0.
 
 ### neo `np.float`
 sPyNNaker 6.0.0 requires neo < 0.10.0, that has a `numpy` compatibility issue (`np.float` is used yet deprecated).
 
 Solved editing the `neo.core.spiketrains` module and replacing `np.float` with `float`
-
-
--- correct_and_install.py
+++ correct_and_install.py
@@ -2,19 +2,26 @@
 import numpy as np
 import os
 
+# Gets the folder of packages
 FOLDER = "/"+np.__file__.strip("numpy/__init__.py")
 FILE = os.path.join(FOLDER, "neo/core/spiketrain.py")
 
+# Opens neo.core.spiketrain
 with open(FILE, "r") as f:
     file_string = f.read()
 
+# Corrects the error
 modified = file_string.replace("np.float", "float")
 
 with open(FILE, "w") as f:
     f.write(modified)
-    
+
+# Check for neo import
 try:
     import neo
 except AttributeError:
-    print("correction of neo didn't go well. quitting.")
+    print("Correction of neo didn't go well. quitting.")
     exit()
+
+## Sets up pyNN.spinnaker
+os.system("python3 -m spynnaker8.setup_pynn")
