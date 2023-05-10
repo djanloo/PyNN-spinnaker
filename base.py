0e1e038ab68e2f639bf04d02c838544a50ecfba3
djanloo <zzibecu@gmail.com>
2023-05-10 23:13:01 +0200

    small experiments

--- base.py
+++ base.py
@@ -1,3 +1,5 @@
+"""Small test for SpikeSourcePoisson
+"""
 import matplotlib
 matplotlib.use('Agg')
 
@@ -6,65 +8,59 @@ import matplotlib.pyplot as plt
 from pyNN.random import NumpyRNG, RandomDistribution
 from pyNN.utility import SimulationProgressBar
 from pyNN.utility.plotting import plot_spiketrains
-import pyNN.spiNNaker as sim
-
-module_dict=dict()
-def module_explorer(object, iter):
-    global module_dict
-    try:
-        obj_name= object.__name__
-        try:
-            desc = object.__dict__
-            if iter > 0:
-                if len(desc) > 0 and not obj_name.startswith("?"):
-                    module_dict[object.__name__] =[sub_obj for sub_obj in list(desc.keys()) if not sub_obj.startswith("?")]#{sub_obj:{"weight":1/iter} for sub_obj in desc.keys() if not sub_obj.startswith("?")}#
-                for sub_obj in desc.values():
-                    module_explorer(sub_obj, iter-1)
-        except Exception as e:
-            pass
-    except Exception:
-        pass
-    print(iter, end=" ")
-module_explorer(sim, 2)
-with open("sim_description.txt", "w") as f:
-    for key in module_dict.keys():
-        f.write(f"{key}\n\t{str(module_dict[key])}\n")
-
-
+import pyNN.brian2 as sim
+from time import perf_counter
 
 simulation_time = 100
+excitatory_strength = 1.0
+inhibitory_strength = 1000.0
+connection_probability = 0.3
+
 dt = 0.1
+N = 10
+
 sim.setup(timestep=dt)
+print("starting 0")
 
 # Neurons
-single_neuron = sim.IF_cond_exp()
-population = sim.Population(10, single_neuron)
-population.record(("v", "spikes"))
+bombing_population = sim.Population(1, sim.SpikeSourcePoisson(rate=500))
+
+target_population = sim.Population(N, sim.IF_cond_alpha())
+target_population.record(("v", "spikes"))
+
+calming_population = sim.Population(N//4, sim.IF_cond_alpha())
 
 # Connections
-synapse = sim.StaticSynapse(weight=0.015, 
-                            delay=0.5)
+bombing_connections = sim.Projection(   bombing_population, target_population, 
+                                sim.AllToAllConnector(), 
+                                sim.StaticSynapse(weight=0.05, delay=0.5), 
+                                receptor_type='excitatory')
 
-random_conn = sim.FixedProbabilityConnector(0.3)
+self_connections = sim.Projection(target_population, target_population,
+                                  sim.FixedProbabilityConnector(connection_probability),
+                                  sim.StaticSynapse(weight=excitatory_strength/N/connection_probability, delay=0.5))
 
-connections = dict(self_connections = sim.Projection(population, population, 
-                                                    random_conn, synapse, 
-                                                    receptor_type='excitatory'))
+calming_connections = sim.Projection(calming_population, target_population,
+                                     sim.AllToAllConnector(),
+                                     sim.StaticSynapse(weight=inhibitory_strength/N, delay=0.8))
 
-# Injection
-current = sim.DCSource(amplitude=5.0, start=0.0, stop=50.0)
-# current = sim.NoisyCurrentSource(mean=5.5, stdev=1.0, start=0.0, stop=450.0, dt=dt)
-current.inject_into(population[0:2])
+calm_stimulating = sim.Projection(   target_population, calming_population,
+                                     sim.AllToAllConnector(),
+                                     sim.StaticSynapse(weight=excitatory_strength/N, delay=0.2))
 
+start = perf_counter()
+print("starting")
 sim.run(simulation_time, callbacks=[SimulationProgressBar(100*dt, simulation_time)])
-print()
+runtime = perf_counter() - start
+print(f"run time: {runtime}")
 
 # Plot data
-block = population.get_data()
+block = target_population.get_data()
 print(f"{len(block.segments)} segments of block found")
 
 fig, (axspike, axv) = plt.subplots(2,1, sharex=True)
 for signal in block.segments[0].analogsignals:
     axv.plot(signal.times, signal.magnitude)
 plot_spiketrains(axspike,block.segments[0].spiketrains )
+axspike.set_title(f"Runtime: {runtime:.3f} s")
 fig.savefig(f"results_{sim.__name__}.png")
