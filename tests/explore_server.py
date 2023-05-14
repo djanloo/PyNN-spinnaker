ca5642cff958e67eab6ea4ac44da3e4cf0b9e5c6
djanloo <zzibecu@gmail.com>
2023-05-14 20:33:24 +0200

    added pipfreeze search

--- tests/explore_server.py
+++ tests/explore_server.py
@@ -6,10 +6,11 @@ env_folder = os.getcwd()
 file_tree = os.path.join(env_folder, 'filetree.txt')
 module_tree = os.path.join(env_folder, 'moduletree.txt')
 installation_log_file = os.path.join(env_folder, 'install_log.txt')
-
+pipfreeze = os.path.join(env_folder, 'pipfreeze.txt')
 # Explore remote folders
 # os.chdir("../")
 os.system(f"python -m dirtree '..' > {file_tree}")
+os.system(f"pip freeze > {pipfreeze}")
 
 # Eplore the remote API
 os.system(f"pip install inspectshow dirtree > {installation_log_file}")
